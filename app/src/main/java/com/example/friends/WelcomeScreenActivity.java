ad807dee4130204b00ac0f3f5535edeb11a66c68
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2019-12-30 22:36:11 +0530

    Welcome Screen added

---
+++ app/src/main/java/com/example/friends/WelcomeScreenActivity.java
@@ -0,0 +1,39 @@
+package com.example.friends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        init();
+    }
+    private void init() {
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent=new Intent(WelcomeScreenActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
